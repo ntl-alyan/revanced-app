ca9066e560538ee1d3997994bf43632fa5980350
ntl-arslan <arslanali.irshad@nayatel.com>
2025-07-27 23:11:15 +0500

    button added

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -3,9 +3,10 @@ import React from 'react';
 
 export default function Page() {
   return (
-    <div className="container mt-5">
-      <h1 className="display-1">This is Bootstrap Heading</h1>
+	<div className="container mt-5">
+	  <h1 className="primary">This is Bootstrap Heading</h1>
+	  <button className='btn btn-primary'>Hello</button>
 
-    </div>
+	</div>
   );
 }
