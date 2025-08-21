fd87d72748714a58c974a1072e52edc30bd73e89
ntl-alyan <alyan.quddoos@nayatel.com>
2025-08-22 01:26:02 +0500

    fixed

---
+++ src/lib/mongo-connect.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+  throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
+}
+
+/**
+ * Global is used to maintain a cached connection across hot reloads in development.
+ */
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function dbConnect() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+      // other mongoose options if needed
+    };
+
+    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => mongoose);
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
+
+export default dbConnect;
